a4e5f448493941e90de580b3c86e90c4f3bc32cd
yifanpu001 <egoisi1948>
2020-04-23 03:38:47 +0800

    debug

--- test_condconv.py
+++ test_condconv.py
@@ -1,9 +1,24 @@
+import torch
+from torch import nn
 from condconv.condconv import CondConv2D
 
 
-input_tensor = torch.rand(2, 4, 224, 224)
-conv_layer = CondConv2d(num_experts=6, in_channels=4, out_channels=8, kernel_size=3, 
+batch_size = 1 # You need update param a sample.
+
+
+class Model(nn.Module):
+    def __init__(self, num_experts):
+        super(Model, self).__init__()
+        self.condconv2d = CondConv2D(10, 128, kernel_size=1, num_experts=num_experts, dropout_rate=dropout_rate)
+        
+    def forward(self, x):
+        x = self.condconv2d(x)
+
+
+input_tensor = torch.rand(1, 3, 224, 224)
+conv_layer = CondConv2D(num_experts=1, in_channels=3, out_channels=6, kernel_size=3, 
                 stride=1, padding=0, dilation=1, groups=1, bias=False,
                 padding_mode='zeros')
 output = conv_layer(input_tensor)
+print(output.shape)
 print("end")
